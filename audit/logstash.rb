de88e85b3f214b889bfa0f1fe448336dc7e38713
Nathan Huff <nrhuff@umn.edu>
2013-09-12 14:34:56 -0500

    Update format for logstash v1.2

--- audit/logstash.rb
+++ audit/logstash.rb
@@ -19,19 +19,19 @@ module MCollective
         request_timezone = request_time.utc? ? 'Z' : request_time.strftime("%z")
         request_time_iso8601 = "%s.%06d%s" % [request_time.strftime("%Y-%m-%dT%H:%M:%S"), request_time.tv_usec, request_timezone]
 
-        audit_entry = {'@source_host' => config.identity,
-                       '@tags' => [],
-                       '@type' => 'mcollective-audit',
-                       '@source' => 'mcollective-audit',
-                       '@timestamp' => now_iso8601,
-                       '@fields' => {'uniqid' => request.uniqid,
-                                     'request_time' => request_time_iso8601,
-                                     'caller' => request.caller,
-                                     'callerhost' => request.sender,
-                                     'agent' => request.agent,
-                                     'action' => request.action,
-                                     'data' => request.data.pretty_print_inspect},
-                       '@message' => "#{config.identity}: #{request.caller}@#{request.sender} invoked agent #{request.agent}##{request.action}"}
+        audit_entry = {'source_host' => config.identity,
+                       'tags' => [],
+                       'type' => 'mcollective-audit',
+                       'source' => 'mcollective-audit',
+                       'mco_timestamp' => now_iso8601,
+                       'uniqid' => request.uniqid,
+                       'request_time' => request_time_iso8601,
+                       'caller' => request.caller,
+                       'callerhost' => request.sender,
+                       'agent' => request.agent,
+                       'action' => request.action,
+                       'data' => request.data.pretty_print_inspect},
+                       'message' => "#{config.identity}: #{request.caller}@#{request.sender} invoked agent #{request.agent}##{request.action}"}
 
         logfile = config.pluginconf.fetch('logstash.logfile', '/var/log/mcollective-audit.log')
 
